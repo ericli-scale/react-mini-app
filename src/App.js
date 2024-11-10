a0bcf92e9f8e78faf76bd72ee25fdae5fdcd1066
Eric Li <eric.li@scale.com>
2024-11-10 15:19:52 -0800

    cleanup and update readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "@/components";
-// import { Button } from "@/components/Button";
+
 const App = () => {
   const [count, setCount] = useState(0);
 
